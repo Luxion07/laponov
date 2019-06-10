d138185f7eeebbe679aba7ee44925bdc8a34e5cd
Luxion <luxion0770@gmail.com>
2019-06-10 14:17:55 +0300

    Add example companies vue js

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -8,6 +8,16 @@
 require('./bootstrap');
 
 window.Vue = require('vue');
+import VueRouter from 'vue-router';
+
+
+window.Vue.use(VueRouter);
+
+
+import CompaniesIndex from './components/companies/CompaniesIndex.vue';
+import CompaniesCreate from './components/companies/CompaniesCreate.vue';
+import CompaniesEdit from './components/companies/CompaniesEdit.vue';
+
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -15,8 +25,18 @@ window.Vue = require('vue');
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-Vue.component('example', require('./components/Example.vue'));
 
-const app = new Vue({
-    el: '#app'
-});
+const routes = [
+    {
+        path: '/',
+        components: {
+            companiesIndex: CompaniesIndex
+        }
+    },
+    {path: '/admin/companies/create', component: CompaniesCreate, name: 'createCompany'},
+    {path: '/admin/companies/edit/:id', component: CompaniesEdit, name: 'editCompany'},
+]
+
+const router = new VueRouter({ routes })
+
+const app = new Vue({ router }).$mount('#app')
